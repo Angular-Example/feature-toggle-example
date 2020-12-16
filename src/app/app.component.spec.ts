f544d6f7ea9bd5c5144efc77a73be6c55e264a40
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-16 11:57:40 -0600

    added some tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { FeatureToggle } from './feature-toggle';
+import { FeatureToggleService } from './feature-toggle.service';
+import { Observable, of } from 'rxjs';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
+    const featureTogglesMock: FeatureToggle[] = [
+      { active: true, name: 'Dr Nice' },
+      { active: false, name: 'Narco' },
+      { active: true, name: 'Bombasto' }
+    ];
+
+    // Create a fake TwainService object with a `getQuote()` spy
+    const featureToggleService = jasmine.createSpyObj('FeatureToggleService', ['getFeatureToggles']);
+    // Make the spy return a synchronous Observable with the test data
+    const getFeatureTogglesSpy = featureToggleService.getFeatureToggles.and.returnValue(of(featureTogglesMock));
+
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      providers: [
+          { provide: ComponentFixtureAutoDetect, useValue: true },
+          { provide: FeatureToggleService, useValue: featureToggleService }
       ],
     }).compileComponents();
   });
@@ -16,16 +35,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tour-of-heroes'`, () => {
+  it(`should have as title 'Feature Toggle Example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heroes');
+    expect(app.title).toEqual('Feature Toggle Example');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain(app.title);
   });
 });
--- src/app/feature-toggle.service.spec.ts
+++ src/app/feature-toggle.service.spec.ts
@@ -1,16 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
 import { FeatureToggleService } from './feature-toggle.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { FeatureToggle } from './feature-toggle';
 
 describe('FeatureToggleService', () => {
+
+  let injector: TestBed;
   let service: FeatureToggleService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HeroService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    injector = getTestBed();
+    service = injector.get(FeatureToggleService);
+    httpMock = injector.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+
+  it('should return an Observable<FeatureToggle[]>', () => {
+    const featureTogglesMock: FeatureToggle[] = [
+      { active: true, name: 'Dr Nice' },
+      { active: false, name: 'Narco' },
+      { active: true, name: 'Bombasto' }
+    ];
+
+    service.getFeatureToggles().subscribe(featureToggles => {
+      expect(featureToggles.length).toBe(3);
+      expect(featureToggles).toEqual(featureTogglesMock);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:8080/feature/list`);
+    expect(req.request.method).toBe("GET");
+    req.flush(featureTogglesMock);
+  });
+
+  afterEach(() => {
+//     httpMock.verify();
   });
 });
